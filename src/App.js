0175cd0e7888b8b27c15a4cd2ae8edb60db4f023
zioson <140989349+zioson@users.noreply.github.com>
2023-08-11 02:08:03 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+//Auth token we will use to generate a meeting and connect to it
+export const authToken = "9feb7fea-7192-40eb-b380-60167e73a2c0";
+// API call to create meeting
+export const createMeeting = async ({ token }) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${authToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+  //Destructuring the roomId from the response
+  const { roomId } = await res.json();
+  return roomId;
+};
+
+import "./App.css";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {
+  MeetingProvider,
+  MeetingConsumer,
+  useMeeting,
+  useParticipant,
+} from "@videosdk.live/react-sdk";
+import { authToken, createMeeting } from "./API";
+import ReactPlayer from "react-player";
+
+function JoinScreen({ getMeetingAndToken }) {
+  return null;
+}
+
+function ParticipantView(props) {
+  return null;
+}
+
+function Controls(props) {
+  return null;
+}
+
+function MeetingView(props) {
+  return null;
+}
+
+function App() {
+  const [meetingId, setMeetingId] = useState(null);
+
+  //Getting the meeting id by calling the api we just wrote
+  const getMeetingAndToken = async (id) => {
+    const meetingId =
+      id == null ? await createMeeting({ token: authToken }) : id;
+    setMeetingId(meetingId);
+  };
+
+  //This will set Meeting Id to null when meeting is left or ended
+  const onMeetingLeave = () => {
+    setMeetingId(null);
+  };
+
+  return authToken && meetingId ? (
+    <MeetingProvider
+      config={{
+        meetingId,
+        micEnabled: true,
+        webcamEnabled: true,
+        name: "C.V. Raman",
+      }}
+      token={authToken}
+    >
+      <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
+    </MeetingProvider>
+  ) : (
+    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
+  );
+}
+
+export default App;
