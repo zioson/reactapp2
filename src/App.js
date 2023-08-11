61b2b202cd65458ff912f46bf78430d4b0404a7b
zioson <140989349+zioson@users.noreply.github.com>
2023-08-11 03:20:10 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,77 +1,101 @@
-//Auth token we will use to generate a meeting and connect to it
-export const authToken = "a9815389-3b6d-4e3b-8f30-f7b5a43d8a97";
-// API call to create meeting
-export const createMeeting = async ({ token }) => {
-  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
-    method: "POST",
-    headers: {
-      authorization: `${authToken}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  });
-  //Destructuring the roomId from the response
-  const { roomId } = await res.json();
-  return roomId;
-};
+import React, { useEffect } from "react";
+import { VideoSDKMeeting } from "@videosdk.live/rtc-js-prebuilt";
 
-import "./App.css";
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import {
-  MeetingProvider,
-  MeetingConsumer,
-  useMeeting,
-  useParticipant,
-} from "@videosdk.live/react-sdk";
-import { authToken, createMeeting } from "./API";
-import ReactPlayer from "react-player";
+export default function App() {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_VIDEOSDK_API_KEY;
+    const meetingId = "milkyway";
+    const name = "Demo User";
 
-function JoinScreen({ getMeetingAndToken }) {
-  return null;
+    const config = {
+      name: name,
+      meetingId: meetingId,
+      apiKey: apiKey,
+
+      region: "sg001", // region for new meeting
+
+      containerId: null,
+      redirectOnLeave: "https://www.videosdk.live/",
+
+      micEnabled: true,
+      webcamEnabled: true,
+      participantCanToggleSelfWebcam: true,
+      participantCanToggleSelfMic: true,
+      participantCanLeave: true, // if false, leave button won't be visible
+
+      chatEnabled: true,
+      screenShareEnabled: true,
+      pollEnabled: true,
+      whiteboardEnabled: true,
+      raiseHandEnabled: true,
+
+      recording: {
+        autoStart: true, // auto start recording on participant joined
+        enabled: true,
+        webhookUrl: "https://www.videosdk.live/callback",
+        awsDirPath: `/meeting-recordings/${meetingId}/`, // automatically save recording in this s3 path
+      },
+
+      livestream: {
+        autoStart: true,
+        enabled: true,
+      },
+
+      layout: {
+        type: "SPOTLIGHT", // "SPOTLIGHT" | "SIDEBAR" | "GRID"
+        priority: "PIN", // "SPEAKER" | "PIN",
+        // gridSize: 3,
+      },
+
+      branding: {
+        enabled: true,
+        logoURL:
+          "https://static.zujonow.com/videosdk.live/videosdk_logo_circle_big.png",
+        name: "Prebuilt",
+        poweredBy: false,
+      },
+
+      permissions: {
+        pin: true,
+        askToJoin: false, // Ask joined participants for entry in meeting
+        toggleParticipantMic: true, // Can toggle other participant's mic
+        toggleParticipantWebcam: true, // Can toggle other participant's webcam
+        drawOnWhiteboard: true, // Can draw on whiteboard
+        toggleWhiteboard: true, // Can toggle whiteboard
+        toggleRecording: true, // Can toggle meeting recording
+        toggleLivestream: true, //can toggle live stream
+        removeParticipant: true, // Can remove participant
+        endMeeting: true, // Can end meeting
+        changeLayout: true, //can change layout
+      },
+
+      joinScreen: {
+        visible: true, // Show the join screen ?
+        title: "Daily scrum", // Meeting title
+        meetingUrl: window.location.href, // Meeting joining url
+      },
+
+      leftScreen: {
+        // visible when redirect on leave not provieded
+        actionButton: {
+          // optional action button
+          label: "Video SDK Live", // action button label
+          href: "https://videosdk.live/", // action button href
+        },
+      },
+
+      notificationSoundEnabled: true,
+
+      debug: true, // pop up error during invalid config or netwrok error
+
+      maxResolution: "sd", // "hd" or "sd"
+
+      // For more features check: /prebuilt/guide/prebuilt-video-and-audio-calling/getting-started
+    };
+
+    const meeting = new VideoSDKMeeting();
+    meeting.init(config);
+  }, []);
+
+  return <div></div>;
 }
-
-function ParticipantView(props) {
-  return null;
-}
-
-function Controls(props) {
-  return null;
-}
-
-function MeetingView(props) {
-  return null;
-}
-
-function App() {
-  const [meetingId, setMeetingId] = useState(null);
-
-  //Getting the meeting id by calling the api we just wrote
-  const getMeetingAndToken = async (id) => {
-    const meetingId =
-      id == null ? await createMeeting({ token: authToken }) : id;
-    setMeetingId(meetingId);
-  };
-
-  //This will set Meeting Id to null when meeting is left or ended
-  const onMeetingLeave = () => {
-    setMeetingId(null);
-  };
-
-  return authToken && meetingId ? (
-    <MeetingProvider
-      config={{
-        meetingId,
-        micEnabled: true,
-        webcamEnabled: true,
-        name: "C.V. Raman",
-      }}
-      token={authToken}
-    >
-      <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
-    </MeetingProvider>
-  ) : (
-    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
-  );
-}
-
-export default App;
